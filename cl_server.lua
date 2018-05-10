8eaf247df49bd4aa502546452fea2251f3a935ae
rhys19 <30848481+rhys19@users.noreply.github.com>
2018-05-09 22:41:39 -0500

    Update cl_server.lua

--- cl_server.lua
+++ cl_server.lua
@@ -1,3 +1,24 @@
+_VERSION = '5.0.2'
+
+-- Version check
+PerformHttpRequest("https://github.com/rhys19/Menu/edit/master/version.txt", function(err, rText, headers)
+	print("\nCurrent version: " .. _VERSION)
+
+	if err == 200 then
+		print("Updater version: " .. rText .. "\n")
+		
+		if rText ~= _VERSION then
+			print("\nVersion mismatch, you are currently not using the newest stable version of Menu. Please update\n")
+			log('Version mismatch was detected, updater version: ' .. rText .. '(' .. _VERSION .. ')')
+		else
+			print("Everything is fine!\n")
+		end
+	else
+		print("Updater version: UPDATER UNAVAILABLE")
+		print("This could be your internet connection or that the update server is not running. This won't impact the server\n\n")
+	end
+end, "GET", "", {what = 'this'})
+
 RegisterServerEvent('wk_actionmenu:cuffGranted')
 AddEventHandler('wk_actionmenu:cuffGranted', function(t)
 	TriggerClientEvent('wk_actionmenu:getArrested', t)
